8bb27972c6185e293f947192df3ba216c57f677d
s.monde <s.monde@edion.co.jp>
2023-01-04 12:33:08 +0900

    provider.tf

---
+++ provider.tf
@@ -0,0 +1,24 @@
+variable "aws_accesskey" {}
+variable "aws_secretkey" {}
+
+provider "aws" {
+  access_key = var.aws_accesskey
+  secret_key = var.aws_secretkey
+  region = "ap-northeast-1"
+}
+
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
+
+terraform {
+  backend "remote" {
+    organization = "mon-test-terraformcloud"
+
+    workspaces {
+      name = "tfc-test"
+    }
+  }
+}
+
