95998d607f91493c1050c9c5b065f416450e25e3
s.monde <s.monde@edion.co.jp>
2023-01-04 14:53:36 +0900

    dev2

---
+++ dev/init/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  type    = string
+  default = "edion"
+}
+
+variable "service_name" {
+  type    = string
+  default = "initsample"
+}
+
+locals {
+  env                     = "stg"
+  env_long                = "staging"
+  resource_prefix         = "${var.project_name}-${var.service_name}-${local.env}"
+}
