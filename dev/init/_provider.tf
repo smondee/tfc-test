14e814a536b5c9a421eb275ef587867ae14e9ba2
s.monde <s.monde@edion.co.jp>
2023-01-05 17:31:26 +0900

    tf

--- dev/init/_provider.tf
+++ dev/init/_provider.tf
@@ -1,15 +1,4 @@
-#variable "aws_accesskey" {}
-#variable "aws_secretkey" {}
-#variable "AWS_ACCESS_KEY_ID" {}
-#variable "AWS_SECRET_ACCESS_KEY" {}
-
 provider "aws" {
-# access_key = var.aws_accesskey
-# secret_key = var.aws_secretkey
-
-#  access_key = var.AWS_ACCESS_KEY_ID
-#  secret_key = var.AWS_SECRET_ACCESS_KEY
-
 
   region = "ap-northeast-1"
 
--- dev/init/terraform.tf
+++ dev/init/terraform.tf
@@ -1,15 +1,17 @@
 #AWS account alias
 resource "aws_iam_account_alias" "alias" {
-  account_alias = "${local.resource_prefix}-alias"
+  account_alias = "${local.resource_prefix}-ali"
 }
 
+#AWS IAM GROUP
 resource "aws_iam_group" "dev" {
   name = "test-kanri-ope"
   path = "/"
 }
 
+#AWS IAM パスワードポリシー
 resource "aws_iam_account_password_policy" "strict" {
-  minimum_password_length        = 8
+  minimum_password_length        = 6
   require_lowercase_characters   = true
   require_numbers                = true
   require_uppercase_characters   = true
