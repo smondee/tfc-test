315b4f570d2220092741a54c5f64df99e214ac1d
s.monde <s.monde@edion.co.jp>
2023-01-05 17:25:59 +0900

    test2

--- dev/init/_provider.tf
+++ dev/init/_provider.tf
@@ -1,14 +1,14 @@
 #variable "aws_accesskey" {}
 #variable "aws_secretkey" {}
 #variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
+#variable "AWS_SECRET_ACCESS_KEY" {}
 
 provider "aws" {
 # access_key = var.aws_accesskey
 # secret_key = var.aws_secretkey
 
-# access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+#  access_key = var.AWS_ACCESS_KEY_ID
+#  secret_key = var.AWS_SECRET_ACCESS_KEY
 
 
   region = "ap-northeast-1"
